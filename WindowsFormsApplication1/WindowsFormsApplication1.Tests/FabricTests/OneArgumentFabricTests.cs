983f9c45832d97c02f9dbb2a92440b99dd8c5684
riddar13 <40752707+riddar13@users.noreply.github.com>
2018-07-04 13:30:42 +0600

    add oneArgumentFactory test

---
+++ WindowsFormsApplication1/WindowsFormsApplication1.Tests/FabricTests/OneArgumentFabricTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using WindowsFormsApplication1.OneArgument;
+using NUnit.Framework;
+
+namespace WindowsFormsApplication1.Tests.FabricTests
+{
+    class OneArgumentFabricTests
+    {
+        [TestCase("Cos", typeof(Cos))]
+        [TestCase("Tan", typeof(Tan))]
+        public void FirstTest(string name, Type type)
+        {
+            var calculator = CalculateOneFactory.CreateCalculator(name);
+            Assert.IsInstanceOf(type, calculator);
+        }
+    }
+}
