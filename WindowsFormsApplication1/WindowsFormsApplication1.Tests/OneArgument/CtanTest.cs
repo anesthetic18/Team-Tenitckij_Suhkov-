7a7c2e6617819749eeb10e97d16170283937bd74
anesthetic18 <40752465+anesthetic18@users.noreply.github.com>
2018-07-04 12:33:02 +0600

    Add Ctan Test

---
+++ WindowsFormsApplication1/WindowsFormsApplication1.Tests/OneArgument/CtanTest.cs
@@ -0,0 +1,19 @@
+﻿using WindowsFormsApplication1.OneArgument;
+using NUnit.Framework;
+
+namespace WindowsFormsApplication1.Tests.OneArgument
+{
+    [TestFixture]
+    public class CtanTest
+    {
+        [TestCase(1, 0.64)]
+        [TestCase(2, -0.46)]
+        [TestCase(3, -7.02)]
+        public void FirstTest(double value, double expected)
+        {
+            ICalculateOneArgument calculator = CalculateOneFactory.CreateCalculator("Ctan");
+            double result = calculator.Calculate(value);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
