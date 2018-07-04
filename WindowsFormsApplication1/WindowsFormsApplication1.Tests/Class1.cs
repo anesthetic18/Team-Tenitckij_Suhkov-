7df445bbec4c3d3f5d2dd55cc6e502815d19738a
riddar13 <40752707+riddar13@users.noreply.github.com>
2018-07-04 11:24:12 +0600

    create projectTests

---
+++ WindowsFormsApplication1/WindowsFormsApplication1.Tests/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace WindowsFormsApplication1.Tests
+{
+    [TestFixture]
+    public class WindowsFormsApplication1Tests
+    {
+        [Test]
+        public void AmountTest()
+        {
+            // подготовительные действия
+            Assert.AreEqual(предполагаемыйРезультат, фактическийРезультат);
+        }
+    }
+}
