82d71198f86d4e65522cb26560f3988e8b11a540
anesthetic18 <40752465+anesthetic18@users.noreply.github.com>
2018-07-04 12:38:54 +0600

    Arcsin

---
+++ WindowsFormsApplication1/WindowsFormsApplication1.Tests/OneArgument/Arcsin.cs
@@ -0,0 +1,19 @@
+﻿using WindowsFormsApplication1.OneArgument;
+using NUnit.Framework;
+
+namespace WindowsFormsApplication1.Tests.OneArgument
+{
+    [TestFixture]
+    public class ArcsinTests
+    {
+        [TestCase(0, 0)]
+        [TestCase(1, 1.57)]
+        [TestCase(-1, -1.57)]
+        public void FirstTest(double value, double expected)
+        {
+            ICalculateOneArgument calculator = CalculateOneFactory.CreateCalculator("Arcsin");
+            double result = calculator.Calculate(value);
+            Assert.AreEqual(expected, result, 0.01);
+        }
+    }
+}
