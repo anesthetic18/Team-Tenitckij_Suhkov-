756823a0f4030bd99605707576ae96826f9ca62e
riddar13 <40752707+riddar13@users.noreply.github.com>
2018-07-04 13:03:27 +0600

    add one more test

---
+++ WindowsFormsApplication1/WindowsFormsApplication1.Tests/TwoArgument/AmountCubeTests.cs
@@ -0,0 +1,20 @@
+﻿using WindowsFormsApplication1.TwoArgument;
+using NUnit.Framework;
+
+namespace WindowsFormsApplication1.Tests.TwoArgument
+{
+    [TestFixture]
+    public class AmountCubeTests
+    {
+        [TestCase(1, 3, 28)]
+        [TestCase(2, 3, 35)]
+        [TestCase(4, 2, 72)]
+        public void FirstTest(double firstValue, double secondValue, double expected)
+        {
+            ICalculateTwoArguments calculator = CalculeteTwoFactory.CreateCalculator("amountCube");
+            double result = calculator.Calculate(firstValue, secondValue);
+            Assert.AreEqual(expected, result, 0.01);
+        }
+    }
+}
+
