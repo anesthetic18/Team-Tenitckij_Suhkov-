9c213e5d9f5469d17ff1b1aa98b4d1da9f99d63e
anesthetic18 <40752465+anesthetic18@users.noreply.github.com>
2018-07-04 13:17:50 +0600

    DoubleDivisionTests

---
+++ WindowsFormsApplication1/WindowsFormsApplication1.Tests/TwoArgument/DoubleDivisionTests.cs
@@ -0,0 +1,19 @@
+﻿using WindowsFormsApplication1.TwoArgument;
+using NUnit.Framework;
+
+namespace WindowsFormsApplication1.Tests.TwoArgument
+{
+    [TestFixture]
+    public class DoubleDivisionTests
+    {
+        [TestCase(1, 1, 2)]
+        [TestCase(2, 2, 2)]
+        [TestCase(3, 3, 2)]
+        public void FirstTest(double firstValue, double secondValue, double expected)
+        {
+            ICalculateTwoArguments calculator = CalculeteTwoFactory.CreateCalculator("doubleDivision");
+            double result = calculator.Calculate(firstValue, secondValue);
+            Assert.AreEqual(expected, result, 0.01);
+        }
+    }
+}
