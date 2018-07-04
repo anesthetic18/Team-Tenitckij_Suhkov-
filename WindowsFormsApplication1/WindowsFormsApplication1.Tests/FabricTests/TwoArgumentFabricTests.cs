14213faa32e00f121add77ee9dce79664e5e3b3f
riddar13 <40752707+riddar13@users.noreply.github.com>
2018-07-04 13:33:56 +0600

    add twoArgumentFabric test

---
+++ WindowsFormsApplication1/WindowsFormsApplication1.Tests/FabricTests/TwoArgumentFabricTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using WindowsFormsApplication1.TwoArgument;
+using NUnit.Framework;
+
+namespace WindowsFormsApplication1.Tests.FabricTests
+{
+    class TwoArgumentFabricTests
+    {
+        [TestCase("DoublAmount", typeof(DoublAmount))]
+        [TestCase("doubleDifference", typeof(DoubleDifference))]
+        public void FirstTest(string name, Type type)
+        {
+            var calculator = CalculeteTwoFactory.CreateCalculator(name);
+            Assert.IsInstanceOf(type, calculator);
+        }
+    }
+}
