c82c1a016866bc1644324642c2b5756e435488e0
riddar13 <40752707+riddar13@users.noreply.github.com>
2018-07-04 12:59:20 +0600

    add tests for difSquare

---
+++ WindowsFormsApplication1/WindowsFormsApplication1.Tests/TwoArgument/DoubleDifSquare.cs
@@ -0,0 +1,20 @@
+﻿using WindowsFormsApplication1.TwoArgument;
+using NUnit.Framework;
+
+namespace WindowsFormsApplication1.Tests.TwoArgument
+{
+    [TestFixture]
+    public class DoubleDifSquareTests
+    {
+        [TestCase(1, 3, -8)]
+        [TestCase(2, 3, -5 )]
+        [TestCase(4, 2, 12)]
+        public void FirstTest(double firstValue, double secondValue, double expected)
+        {
+            ICalculateTwoArguments calculator = CalculeteTwoFactory.CreateCalculator("doubleDifSquare");
+            double result = calculator.Calculate(firstValue, secondValue);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
+
