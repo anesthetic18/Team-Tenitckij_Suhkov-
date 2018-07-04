198dcd9b7c555425f0f83b19588ac00da4ed6104
riddar13 <40752707+riddar13@users.noreply.github.com>
2018-07-04 12:48:11 +0600

    add tests for tan and cube

---
+++ WindowsFormsApplication1/WindowsFormsApplication1.Tests/OneArgument/CubeTests.cs
@@ -0,0 +1,20 @@
+﻿using WindowsFormsApplication1.OneArgument;
+using NUnit.Framework;
+
+namespace WindowsFormsApplication1.Tests.OneArgument
+{
+    [TestFixture]
+    public class CubeTests
+    {
+        [TestCase(1, 1)]
+        [TestCase(2, 8)]
+        [TestCase(3, 27)]
+        public void FirstTest(double value, double expected)
+        {
+            ICalculateOneArgument calculator = CalculateOneFactory.CreateCalculator("Cube");
+            double result = calculator.Calculate(value);
+            Assert.AreEqual(expected, result, 0.01);
+        }
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1.Tests/OneArgument/TanTests.cs
@@ -0,0 +1,21 @@
+﻿using WindowsFormsApplication1.OneArgument;
+using NUnit.Framework;
+
+namespace WindowsFormsApplication1.Tests.OneArgument
+{
+    [TestFixture]
+    public class TanTests
+    {
+        [TestCase(1, 1.55)]
+        [TestCase(2, -2.18)]
+        [TestCase(3, -0.14)]
+        public void FirstTest(double value, double expected)
+        {
+            ICalculateOneArgument calculator = CalculateOneFactory.CreateCalculator("Tan");
+            double result = calculator.Calculate(value);
+            Assert.AreEqual(expected, result, 0.01);
+        }
+    }
+}
+
+
