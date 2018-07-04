958835d0e86e6be0291c163b9c8323c382a96bcc
riddar13 <40752707+riddar13@users.noreply.github.com>
2018-07-04 13:05:06 +0600

    add difCube test

---
+++ WindowsFormsApplication1/WindowsFormsApplication1.Tests/TwoArgument/DifCubeTests.cs
@@ -0,0 +1,21 @@
+﻿using WindowsFormsApplication1.TwoArgument;
+using NUnit.Framework;
+
+namespace WindowsFormsApplication1.Tests.TwoArgument
+{
+    [TestFixture]
+    public class DifCubeTests
+    {
+        [TestCase(1, 3, -26)]
+        [TestCase(2, 3, -19)]
+        [TestCase(4, 2, 56)]
+        public void FirstTest(double firstValue, double secondValue, double expected)
+        {
+            ICalculateTwoArguments calculator = CalculeteTwoFactory.CreateCalculator("difCube");
+            double result = calculator.Calculate(firstValue, secondValue);
+            Assert.AreEqual(expected, result, 0.01);
+        }
+    }
+}
+
+
