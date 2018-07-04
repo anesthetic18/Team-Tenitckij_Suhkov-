ea58eb83840c6c7971abaef61e44fbb09e2a1289
riddar13 <40752707+riddar13@users.noreply.github.com>
2018-07-04 12:51:28 +0600

    add two tests more

---
+++ WindowsFormsApplication1/WindowsFormsApplication1.Tests/OneArgument/AtanTests.cs
@@ -0,0 +1,21 @@
+﻿using WindowsFormsApplication1.OneArgument;
+using NUnit.Framework;
+
+namespace WindowsFormsApplication1.Tests.OneArgument
+{
+    [TestFixture]
+    public class AtanTests
+    {
+        [TestCase(1, 0.78)]
+        [TestCase(2, 1.11)]
+        [TestCase(3, 1.25)]
+        public void FirstTest(double value, double expected)
+        {
+            ICalculateOneArgument calculator = CalculateOneFactory.CreateCalculator("Atan");
+            double result = calculator.Calculate(value);
+            Assert.AreEqual(expected, result, 0.01);
+        }
+    }
+}
+
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1.Tests/OneArgument/FractionTests.cs
@@ -0,0 +1,20 @@
+﻿using WindowsFormsApplication1.OneArgument;
+using NUnit.Framework;
+
+namespace WindowsFormsApplication1.Tests.OneArgument
+{
+    [TestFixture]
+    public class FractionTests
+    {
+        [TestCase(1, 1)]
+        [TestCase(2, 0.5)]
+        [TestCase(3, 0.33)]
+        public void FirstTest(double value, double expected)
+        {
+            ICalculateOneArgument calculator = CalculateOneFactory.CreateCalculator("Fraction");
+            double result = calculator.Calculate(value);
+            Assert.AreEqual(expected, result, 0.01);
+        }
+    }
+}
+
