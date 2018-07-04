c86ed1bcdbeee5a742e13a4e82d2bffb752848f2
anesthetic18 <40752465+anesthetic18@users.noreply.github.com>
2018-07-04 12:27:28 +0600

    Add test TenExp

---
+++ WindowsFormsApplication1/WindowsFormsApplication1.Tests/OneArgument/TenExpTests.cs
@@ -0,0 +1,19 @@
+﻿using WindowsFormsApplication1.OneArgument;
+using NUnit.Framework;
+
+namespace WindowsFormsApplication1.Tests.OneArgument
+{
+    [TestFixture]
+    public class TenExpTests
+    {
+        [TestCase(1, 10)]
+        [TestCase(2, 100)]
+        [TestCase(0, 1)]
+        public void FirstTest(double value, double expected)
+        {
+            ICalculateOneArgument calculator = CalculateOneFactory.CreateCalculator("TenExp");
+            double result = calculator.Calculate(value);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
