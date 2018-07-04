e8e20a465802c9ef8ee7306ae15a467acf574cc9
riddar13 <40752707+riddar13@users.noreply.github.com>
2018-07-04 13:06:42 +0600

    add one more test

---
+++ WindowsFormsApplication1/WindowsFormsApplication1.Tests/TwoArgument/SquareOfAmountTests.cs
@@ -0,0 +1,21 @@
+﻿using WindowsFormsApplication1.TwoArgument;
+using NUnit.Framework;
+
+namespace WindowsFormsApplication1.Tests.TwoArgument
+{
+    [TestFixture]
+    public class SquareOfAmountTests
+    {
+        [TestCase(1, 3, 16)]
+        [TestCase(2, 3, 25)]
+        [TestCase(4, 2, 36)]
+        public void FirstTest(double firstValue, double secondValue, double expected)
+        {
+            ICalculateTwoArguments calculator = CalculeteTwoFactory.CreateCalculator("squareOfAmount");
+            double result = calculator.Calculate(firstValue, secondValue);
+            Assert.AreEqual(expected, result, 0.01);
+        }
+    }
+}
+
+
