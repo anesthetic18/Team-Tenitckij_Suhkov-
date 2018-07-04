ff0d11437002227e4439b11125514ba4a807d9de
anesthetic18 <40752465+anesthetic18@users.noreply.github.com>
2018-07-04 13:14:55 +0600

    DoubleMultiplyTests

---
+++ WindowsFormsApplication1/WindowsFormsApplication1.Tests/TwoArgument/DoubleMultiplyTests.cs
@@ -0,0 +1,19 @@
+﻿using WindowsFormsApplication1.TwoArgument;
+using NUnit.Framework;
+
+namespace WindowsFormsApplication1.Tests.TwoArgument
+{
+    [TestFixture]
+    public class DoubleMultoplyTests
+    {
+        [TestCase(0, 0, 0)]
+        [TestCase(1, 1, 2)]
+        [TestCase(2, 2, 8)]
+        public void FirstTest(double firstValue, double secondValue, double expected)
+        {
+            ICalculateTwoArguments calculator = CalculeteTwoFactory.CreateCalculator("doubleMultiply");
+            double result = calculator.Calculate(firstValue, secondValue);
+            Assert.AreEqual(expected, result, 0.01);
+        }
+    }
+}
