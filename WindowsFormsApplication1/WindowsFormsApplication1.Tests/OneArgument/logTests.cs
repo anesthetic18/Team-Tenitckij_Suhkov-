1fa6d28180afbee9be30653fd46f01f5d465c380
anesthetic18 <40752465+anesthetic18@users.noreply.github.com>
2018-07-04 12:43:35 +0600

    LogTests

---
+++ WindowsFormsApplication1/WindowsFormsApplication1.Tests/OneArgument/logTests.cs
@@ -0,0 +1,19 @@
+﻿using WindowsFormsApplication1.OneArgument;
+using NUnit.Framework;
+
+namespace WindowsFormsApplication1.Tests.OneArgument
+{
+    [TestFixture]
+    public class LogTests
+    {
+        [TestCase(1, 0)]
+        [TestCase(2, 0.69)]
+        [TestCase(3, 1.09)]
+        public void FirstTest(double value, double expected)
+        {
+            ICalculateOneArgument calculator = CalculateOneFactory.CreateCalculator("Log");
+            double result = calculator.Calculate(value);
+            Assert.AreEqual(expected, result, 0.01);
+        }
+    }
+}
