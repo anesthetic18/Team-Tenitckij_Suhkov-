8b9f8bb786ba4483ccd4fefd228268e37048d360
anesthetic18 <40752465+anesthetic18@users.noreply.github.com>
2018-07-04 13:12:25 +0600

    DoubleDifferenceTests

---
+++ WindowsFormsApplication1/WindowsFormsApplication1.Tests/TwoArgument/DoubleDifferenceTests.cs
@@ -0,0 +1,19 @@
+﻿using WindowsFormsApplication1.TwoArgument;
+using NUnit.Framework;
+
+namespace WindowsFormsApplication1.Tests.TwoArgument
+{
+    [TestFixture]
+    public class DoubleDifferenceTests
+    {
+        [TestCase(1, 1, 0)]
+        [TestCase(2, 2, 0)]
+        [TestCase(3, 3, 0)]
+        public void FirstTest(double firstValue, double secondValue, double expected)
+        {
+            ICalculateTwoArguments calculator = CalculeteTwoFactory.CreateCalculator("doubleDifference");
+            double result = calculator.Calculate(firstValue, secondValue);
+            Assert.AreEqual(expected, result, 0.01);
+        }
+    }
+}
