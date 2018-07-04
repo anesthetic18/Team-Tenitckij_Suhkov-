f2c2de17c2bf66420d6ba04afbf0afcea312d3e9
riddar13 <40752707+riddar13@users.noreply.github.com>
2018-07-04 13:01:33 +0600

    add difDivision Test

---
+++ WindowsFormsApplication1/WindowsFormsApplication1.Tests/TwoArgument/DifDivisionTests.cs
@@ -0,0 +1,20 @@
+﻿using WindowsFormsApplication1.TwoArgument;
+using NUnit.Framework;
+
+namespace WindowsFormsApplication1.Tests.TwoArgument
+{
+    [TestFixture]
+    public class DifDivisionTests
+    {
+        [TestCase(1, 3, -2.67)]
+        [TestCase(2, 3, -0.83)]
+        [TestCase(4, 2, 1.5)]
+        public void FirstTest(double firstValue, double secondValue, double expected)
+        {
+            ICalculateTwoArguments calculator = CalculeteTwoFactory.CreateCalculator("difDivision");
+            double result = calculator.Calculate(firstValue, secondValue);
+            Assert.AreEqual(expected, result, 0.01);
+        }
+    }
+}
+
