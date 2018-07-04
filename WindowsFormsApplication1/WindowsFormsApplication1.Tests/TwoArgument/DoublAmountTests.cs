24408d5784ac5f74d53d403efeae8b7b05576c5b
anesthetic18 <40752465+anesthetic18@users.noreply.github.com>
2018-07-04 13:09:39 +0600

    DoublAmountTests

---
+++ WindowsFormsApplication1/WindowsFormsApplication1.Tests/TwoArgument/DoublAmountTests.cs
@@ -0,0 +1,20 @@
+﻿using WindowsFormsApplication1.TwoArgument;
+using NUnit.Framework;
+
+namespace WindowsFormsApplication1.Tests.TwoArgument
+{
+    [TestFixture]
+    public class DoublAmountTests
+    {
+        [TestCase(1, 1, 4)]
+        [TestCase(2, 2, 8)]
+        [TestCase(0, 0,0)]
+        public void FirstTest(double firstValue, double secondValue, double expected)
+        {
+            ICalculateTwoArguments calculator = CalculeteTwoFactory.CreateCalculator("DoublAmount");
+            double result = calculator.Calculate(firstValue, secondValue);
+            Assert.AreEqual(expected, result, 0.01);
+        }
+    }
+}
+
