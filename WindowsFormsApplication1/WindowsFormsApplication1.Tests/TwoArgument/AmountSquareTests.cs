713ec75b6387d91d0a360045edb327a2e4bf26b5
anesthetic18 <40752465+anesthetic18@users.noreply.github.com>
2018-07-04 13:20:46 +0600

    AmountSquareTests

---
+++ WindowsFormsApplication1/WindowsFormsApplication1.Tests/TwoArgument/AmountSquareTests.cs
@@ -0,0 +1,19 @@
+﻿using WindowsFormsApplication1.TwoArgument;
+using NUnit.Framework;
+
+namespace WindowsFormsApplication1.Tests.TwoArgument
+{
+    [TestFixture]
+    public class AmountSquareTests
+    {
+        [TestCase(0, 0, 0)]
+        [TestCase(1, 1, 2)]
+        [TestCase(2, 2, 8)]
+        public void FirstTest(double firstValue, double secondValue, double expected)
+        {
+            ICalculateTwoArguments calculator = CalculeteTwoFactory.CreateCalculator("amountSquare");
+            double result = calculator.Calculate(firstValue, secondValue);
+            Assert.AreEqual(expected, result, 0.01);
+        }
+    }
+}
