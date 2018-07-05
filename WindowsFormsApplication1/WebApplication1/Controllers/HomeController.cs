d76e64b40a45230be650fe44d5157444cfb43cc7
riddar13 <40752707+riddar13@users.noreply.github.com>
2018-07-05 14:29:23 +0600

    create webInterfeys

---
+++ WindowsFormsApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WindowsFormsApplication1.TwoArgument;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Operation = new SelectListItem[]
+            {
+                new SelectListItem()
+                {
+                    Value = "buttonAmount",
+                    Text = "Amount"
+                },
+                new SelectListItem()
+                {
+                    Value = "buttonDifference",
+                    Text = "Difference"
+                }
+            };
+            return View();   
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(double firstArgument, double secondArgument, string operation)
+        {
+            ICalculateTwoArguments calculator = CalculeteTwoFactory.CreateCalculator(operation);
+            double result = calculator.Calculate(firstArgument, secondArgument);
+            ViewBag.Result = result;
+            ViewBag.Operation = new SelectListItem[]
+            {
+                new SelectListItem()
+                {
+                    Value = "buttonAmount",
+                    Text = "Amount"
+                },
+                new SelectListItem()
+                {
+                    Value = "buttonDifference",
+                    Text = "Difference"
+                }
+            };
+            return View();
+        }
+    }
+}
