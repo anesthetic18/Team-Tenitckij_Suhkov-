3442580fa9e90ef2e542bf80b8d605aa2bcf233c
riddar13 <40752707+riddar13@users.noreply.github.com>
2018-07-05 10:39:37 +0600

    add comments

--- WindowsFormsApplication1/WindowsFormsApplication1/OneArgument/CalculateOneArgument.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/OneArgument/CalculateOneArgument.cs
@@ -1,5 +1,8 @@
 ﻿namespace WindowsFormsApplication1.OneArgument
 {
+    /// <summary>
+    /// общий интерфейс для функций с одной переменной
+    /// </summary>
     public interface ICalculateOneArgument
     {
         double Calculate(double argument);
