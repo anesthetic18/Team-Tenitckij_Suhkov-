69e780924da2c304866dcda6013cd686848a3932
riddar13 <40752707+riddar13@users.noreply.github.com>
2018-07-05 10:32:42 +0600

    add obrabitchik mistakes

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -13,23 +13,38 @@ namespace WindowsFormsApplication1
         }
         private void Click(object sender, EventArgs e)
         {
-            string firstValueText = textBox1.Text;
-            string secondValueText = textBox2.Text;
-            double firstValue = Convert.ToDouble(firstValueText);
-            double secondValue = Convert.ToDouble(secondValueText);
-            string buttonName = ((Button)sender).Name;
-            ICalculateTwoArguments calculator = CalculeteTwoFactory.CreateCalculator(buttonName);
-            double result = calculator.Calculate(firstValue, secondValue);
-            textBox3.Text = result.ToString();
+            try
+            {
+                string firstValueText = textBox1.Text;
+                string secondValueText = textBox2.Text;
+                double firstValue = Convert.ToDouble(firstValueText);
+                double secondValue = Convert.ToDouble(secondValueText);
+                string buttonName = ((Button)sender).Name;
+                ICalculateTwoArguments calculator = CalculeteTwoFactory.CreateCalculator(buttonName);
+                double result = calculator.Calculate(firstValue, secondValue);
+                textBox3.Text = result.ToString();
+            }
+            catch (Exception exc)
+            {
+                textBox3.Text = "mistake";
+            }
+         
         }
         private void ClickUnary(object sender, EventArgs e)
         {
-            string firstValueText = textBox1.Text;
-            double firstValue = Convert.ToDouble(firstValueText);
-            string buttonName = ((Button)sender).Name;
-            ICalculateOneArgument calculator = CalculateOneFactory.CreateCalculator(buttonName);
-            double result = calculator.Calculate(firstValue);
-            textBox3.Text = result.ToString();
+            try
+            {
+                string firstValueText = textBox1.Text;
+                double firstValue = Convert.ToDouble(firstValueText);
+                string buttonName = ((Button) sender).Name;
+                ICalculateOneArgument calculator = CalculateOneFactory.CreateCalculator(buttonName);
+                double result = calculator.Calculate(firstValue);
+                textBox3.Text = result.ToString();
+            }
+            catch
+            {
+                textBox3.Text = "mistake";
+            }
         }
     }
 }
